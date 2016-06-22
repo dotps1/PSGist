f52c7cee1971d4376127c4c34f883eccfa3c769c
Thomas Malkewitz <get-alias@outlook.com>
2016-06-22 09:05:53 -0400

    MAJOR OVERHAUL TO AUTHENTICATION
    The module no longer authenticates with a persistant env variable. the
    users OAuth Token is not encrypted and serialized to disk. Because of
    this, Set-GistOAuthtoken has been removed. As well as the Description
    Parameter on New-GistOAuthToken becuase it uses that value for
    validation.

--- Tests/Set-GistOAuthToken.Tests.ps1
+++
@@ -1,9 +0,0 @@
-Describe 'Set-GistOAuthToken' { 
-    Context "Invoke-ScriptAnalyzer -Path $(Resolve-Path -Path (Get-Location))\Public\Set-GistOAuthToken.ps1." {
-        $results = Invoke-ScriptAnalyzer -Path .\PSGist\Public\Set-GistOAuthToken.ps1
-
-        It 'Invoke-ScriptAnalyzer results of Set-GistOAuthToken count should be 0.' {
-            $results.Count | Should Be 0
-        }
-    }
-}
